21f61f5be9dfd021e145aa35e7be72e15e4accb2
Alex Cuni <82770319+acunidev@users.noreply.github.com>
2023-01-12 21:37:48 +0100

    First commit

---
+++ app/src/main/java/com/example/toolbarproject/ActBuscador.java
@@ -0,0 +1,142 @@
+package com.example.toolbarproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.SearchView.OnQueryTextListener;
+import androidx.appcompat.widget.Toolbar;
+import java.io.File;
+
+public class ActBuscador extends AppCompatActivity {
+
+  private Toolbar toolbar1;
+  private ListView listView;
+  private String nomFile;
+  private ArrayAdapter<String> arrayAdapter;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_act_buscador);
+    initItems();
+
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menutoolbarsearch, menu);
+    MenuItem menuItem = menu.findItem(R.id.test1);
+    SearchView searchView = (SearchView) menuItem.getActionView();
+    searchView.setQueryHint("h");
+    searchView.setQueryHint("Escriu aqui per buscar");
+
+    searchView.setOnQueryTextListener(new OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        return false;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        arrayAdapter.getFilter().filter(newText);
+        return false;
+      }
+    });
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.userProfile:
+        startActivity(new Intent(this, PerfilUsuari.class));
+        return true;
+      case R.id.searcher:
+        startActivity(new Intent(this, ActBuscador.class));
+        return true;
+      case R.id.test1:
+        startActivity(new Intent(this, ActBuscador.class));
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void initItems() {
+    listView = findViewById(R.id.listView);
+    File f = new File("randomWords.txt");
+    if (f.exists()) {
+      Log.i("existe", "existe");
+    } else {
+      Log.i("no existe", "no existe");
+    }
+
+    String[] randomWords = {
+        "pillow",
+        "cash",
+        "abolish",
+        "technique",
+        "budge",
+        "winner",
+        "access",
+        "fair",
+        "architecture",
+        "security",
+        "halt",
+        "element",
+        "mug",
+        "kidney",
+        "counter",
+        "tool",
+        "rider",
+        "scatter",
+        "objective",
+        "drink",
+        "sport",
+        "kneel",
+        "bury",
+        "workshop",
+        "posture",
+        "unit",
+        "barrel",
+        "bolt",
+        "quit",
+        "advantage",
+        "graphic",
+        "bill",
+        "appreciate",
+        "herb",
+        "favor",
+        "trail",
+        "urgency",
+        "transparent",
+        "final",
+        "pill",
+        "floor",
+        "brand",
+        "horror",
+        "bark",
+        "scheme",
+        "jelly",
+        "fitness",
+        "accent",
+        "interest",
+        "article"
+    };
+
+    arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, randomWords);
+    listView.setAdapter(arrayAdapter);
+
+    toolbar1 = findViewById(R.id.toolbarSearch);
+    setSupportActionBar(toolbar1);
+  }
+
+}
---
+++ app/src/main/java/com/example/toolbarproject/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.toolbarproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+  private Toolbar toolbar1;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    initItems();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menutoolbar, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.userProfile:
+        startActivity(new Intent(this, PerfilUsuari.class));
+        return true;
+      case R.id.searcher:
+        startActivity(new Intent(this, ActBuscador.class));
+        return true;
+      case R.id.test1:
+        startActivity(new Intent(this, ActBuscador.class));
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void initItems() {
+    toolbar1 = findViewById(R.id.toolbar1);
+    setSupportActionBar(toolbar1);
+  }
+}
